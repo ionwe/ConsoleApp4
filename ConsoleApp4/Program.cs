da619284412c615dfc5295f796b8661b08654f70
Naddfe Crewwe <vavilovalexey2@gmail.com>
2024-10-21 11:37:26 +0500

    huh

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp4
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int number = 12345;
+        int reversedNumber = ReverseNumber(number);
+        Console.WriteLine("Reversed number: " + reversedNumber);
+    }
+
+    static int ReverseNumber(int num)
+    {
+        if (num < 10)
         {
+            return num;
         }
+
+        int lastDigit = num % 10; 
+        int remainingDigits = num / 10; 
+
+        return (int)(lastDigit * Math.Pow(10, (int)Math.Log10(remainingDigits) + 1) + ReverseNumber(remainingDigits));
     }
 }
